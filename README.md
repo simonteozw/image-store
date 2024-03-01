8e5cae56f5443697f3565afca2f588361f175390
simonteozw <simonteozw@gmail.com>
2024-03-01 19:33:52 +0800

    Set up basic redis cache

--- README.md
+++ README.md
@@ -5,4 +5,5 @@ Simple image serving site to learn about redis
 ## Setup
 
 1. Set up virtual env
-2. Set up private_info.json with the following template `{"serp_api_key": <your_api_key>}`
+2. Set up private_info.json with the following template `{"serp_api_key": <your_api_key>, "redis_host": <your_redis_host>, "redis_port": <your_redis_port>, "redis_password": <your_redis_password>}`
+3. run `uvicorn main:app --reload`
--- main.py
+++ main.py
@@ -1,7 +1,13 @@
 import requests
+import redis
 import json
 from fastapi import FastAPI
 import serpapi
+
+with open('private_info.json') as f:
+    data = json.load(f)
+
+rd = redis.Redis(host=data["redis_host"], port=data["redis_port"], password=data["redis_password"], decode_responses=True)
 
 app = FastAPI()
 
@@ -11,15 +17,23 @@ def read_root():
 
 
 @app.get("/images/{thing}")
-def read_fish(thing):
-  with open('private_info.json') as f:
-    data = json.load(f)
-  params = {
-    "engine": "google_images",
-    "q": thing,
-    "api_key": data["serp_api_key"]
-  }
-  print(data["serp_api_key"])
-  search = serpapi.search(params)
-  results = search["images_results"]
-  return results
+def query_image(thing):
+  cache = rd.get(thing)
+  if cache:
+    print("cache hit")
+    results = cache
+  else:
+    print("cache miss")
+    params = {
+      "engine": "google_images",
+      "q": thing,
+      "api_key": data["serp_api_key"]
+    }
+    search = serpapi.search(params)
+    results = []
+    for result in search["images_results"]:
+      results.append({"thumbnail_key": result['thumbnail'], "title": result['title'], "link": result['link']})
+    results = json.dumps(results)
+    rd.set(thing, results)
+    rd.expire(thing, 10)
+  return json.loads(results)
